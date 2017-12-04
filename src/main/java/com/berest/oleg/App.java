f419540cfa4052afb58d3ac8999e7e5c1c99a381
Oleg Berest <oleg.berest@NetCracker.com>
2017-12-04 12:24:22 +0200

    Initial commit

---
+++ src/main/java/com/berest/oleg/App.java
@@ -0,0 +1,15 @@
+package com.berest.oleg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static void fasf(String[] args) {
+        System.out.println("Hello World!")  ;
+    }
+}
